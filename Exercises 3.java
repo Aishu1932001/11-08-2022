cd917421afb5bb767ae107ebabf0d4ebb853e6ca
Aishwarya D <110168081+Aishu1932001@users.noreply.github.com>
2022-08-12 09:33:18 +0530

    Add files via upload

---
+++ Exercises 3.java
@@ -0,0 +1,60 @@
+package com.exercises 3.demo;
+
+
+import java.util.ArrayList;
+    
+    import java.util.Scanner;
+    
+    public class Exercises 3 { 	
+        
+        
+        public static void main(String[] args) {			
+        
+        
+        ArrayList<String> words = new ArrayList<>();			
+        
+        words.add("Hello");	
+        
+        words.add("Book");	
+        
+        words.add("Cable");		
+        
+        words.add("Home");		
+        
+        words.add("Class");	
+        
+        words.add("Pen");	
+        
+        words.add("Phone");	
+        
+        words.add("Apple");	
+        
+        words.add("Orenge");	
+        
+        words.add("Table");		
+        
+        findWord(words); 
+    }		
+    
+    public static void findWord(ArrayList<String> words) {		
+        
+        
+        Scanner scanner = new Scanner(System.in);		
+        
+        System.out.println("Enter the word length: ");			
+        
+        int length = scanner.nextInt();		
+        
+        
+        for(String word : words) {			
+            
+            
+            if(word.length() == length) {						
+                
+                
+                System.out.println(word);			
+            }	
+        }		
+        
+        scanner.close();	
+    }
